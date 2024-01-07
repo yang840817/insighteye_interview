d8f58ee0949e2b94c8e409dee245dabc00366bc2
“Tim <yang840817@gmail.com>
2024-01-07 19:15:00 +0800

    fix: fill api baseURL

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,8 +7,12 @@ import axios from "axios";
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: "https://api.example.com" });
-
+const api = axios.create({
+  baseURL: "http://35.194.177.50:7777",
+  headers: {
+    Accept: "application/json",
+  },
+});
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
 
