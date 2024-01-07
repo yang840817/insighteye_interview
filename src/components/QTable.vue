7a198f4e3589e72c06f8aaf15374b2b22200d079
“Tim <yang840817@gmail.com>
2024-01-07 19:22:12 +0800

    feat: complete employee information page
    1. fetch employee data from api
    2. features:Add/Delete/Sort/Search
    3. features:table cell hover tooltip

--- src/components/QTable.vue
+++ src/components/QTable.vue
@@ -4,32 +4,76 @@
     row-key="name"
     :columns="columns"
     :rows="rows"
+    :filter="filter"
     separator="cell"
     flat
     bordered
-  />
+  >
+    <template v-slot:top-right>
+      <q-input
+        borderless
+        dense
+        debounce="1000"
+        placeholder="Search"
+        :model-value="filter"
+        @update:model-value="$emit('onFilter', $event)"
+      >
+        <template v-slot:append>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+    </template>
+    <template v-slot:body-cell="props">
+      <q-td :props="props">
+        {{ props.value }}
+        <q-tooltip
+          class="text-caption"
+          anchor="bottom middle"
+          self="bottom middle"
+        >
+          {{ props.value }}
+        </q-tooltip>
+      </q-td>
+    </template>
+    <template v-slot:pagination="scope">
+      <q-btn
+        icon="chevron_left"
+        color="grey-8"
+        round
+        dense
+        flat
+        :disable="scope.isFirstPage"
+        @click="scope.prevPage"
+      ></q-btn>
+      {{ scope.pagination.page }}
+      <q-btn
+        icon="chevron_right"
+        color="grey-8"
+        round
+        dense
+        flat
+        :disable="scope.isLastPage"
+        @click="scope.nextPage"
+      ></q-btn>
+      {{ `共${rows.length}筆資料` }}
+    </template>
+  </q-table>
 </template>
-<script>
-import { onMounted } from "vue";
-
-export default {
-  props: {
+<script setup>
+const props = defineProps({
     columns: {
       type: Array,
       default: () => [],
     },
-
     rows: {
       type: Array,
       default: () => [],
     },
-  },
-
-  setup(props, { emit }) {
-    onMounted(() => {});
-
-    return {};
-  },
-};
+    filter: {
+      type: String,
+      default: () => "",
+    },
+});
+const emit = defineEmits(["onFilter"]);
 </script>
 <style lang="scss" scoped></style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,68 +4,217 @@
       <div class="row justify-between items-center">
         <div class="text-h6">員工基本資訊</div>
         <div>
-          <q-btn class="q-mr-sm" color="primary" label="新增" />
-          <q-btn color="white" text-color="black" label="刪除" />
+          <q-btn
+            class="q-mr-sm"
+            color="primary"
+            label="新增"
+            @click="isAddDialog = true"
+          />
+          <q-btn
+            color="white"
+            text-color="black"
+            label="刪除"
+            :disable="!selected.length"
+            @click="isDeleteDialog = true"
+          />
         </div>
       </div>
-      <QTable :columns="state.columns" />
+      <QTable
+        :columns="state.columns"
+        :rows="state.rows"
+        v-model:selected="selected"
+        selection="multiple"
+        :selected-rows-label="getSelectedString"
+        rows-per-page-label="每頁筆數"
+        :rows-per-page-options="[10, 20, 30]"
+        :loading="loading"
+        :filter="filter"
+        binary-state-sort
+        @onFilter="onFilter"
+      >
+      </QTable>
+      <AddDialog
+        v-model:isAddDialog="isAddDialog"
+        @onCancel="onCancelAdd"
+        @onConfirm="onConfirmAdd"
+      />
+      <DeleteDialog
+        v-model:isDeleteDialog="isDeleteDialog"
+        v-model:selected="selected"
+        @onCancel="onCancelDelete"
+        @onConfirm="onConfirmDelete"
+      />
     </div>
   </q-page>
 </template>
-
-<script>
-import { reactive } from "vue";
+<script setup>
+import { ref, reactive, onMounted } from "vue";
 import QTable from "src/components/QTable.vue";
+import DeleteDialog from "src/components/DeleteDialog.vue";
+import AddDialog from "src/components/AddDialog.vue";
+import { api } from "src/boot/axios.js";
+import { date } from "quasar";
 
-export default {
-  name: "IndexPage",
+const selected = ref([]);
+const isAddDialog = ref(false);
+const isDeleteDialog = ref(false);
+const filter = ref("");
+const loading = ref(false);
+const originData = ref([]);
+const state = reactive({
+  columns: [
+    {
+      name: "name",
+      label: "姓名",
+      align: "left",
+      field: "name",
+      sortable: true,
+    },
+    {
+      name: "cellphone",
+      label: "手機",
+      align: "left",
+      field: "cellphone",
+      sortable: true,
+    },
+    {
+      name: "email",
+      label: "信箱",
+      align: "left",
+      field: "email",
+      sortable: true,
+    },
+    {
+      name: "gender",
+      label: "性別",
+      align: "left",
+      field: "gender",
+      sortable: true,
+    },
+    {
+      name: "birthday",
+      label: "生日",
+      align: "left",
+      field: "birthday",
+      sortable: true,
+    },
+  ],
+  rows: [],
+});
 
-  components: {
-    QTable,
-  },
+onMounted(async () => {
+  loading.value = true;
+  const data = await fetchMembers();
+  originData.value = data.map((item) => {
+    return { ...item, birthday: formatBirthday(item.birthday) };
+  });
+  loading.value = false;
+  state.rows = [...originData.value];
+});
 
-  setup() {
-    const state = reactive({
-      columns: [
-        {
-          name: "name",
-          label: "姓名",
-          align: "left",
-          field: "name",
+async function fetchMembers() {
+  try {
+    const { data } = await api.get(`/members`);
+    return data.members;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
+async function fetchFilterMembers(
+  requestPayload = {
+    filter: {},
+    sort: "",
+  }
+) {
+  try {
+    const { data } = await api.post(`/members/search`, requestPayload);
+    return data.members;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
+function fetchFromServer(filter) {
+  // 依column分別call api搜尋，生日格式特殊不搜尋
+  const filterKeyList = ["name", "cellphone", "email", "gender"];
+  if (filter) {
+    return Promise.all(
+      filterKeyList.map((key) => {
+        const payload = {
+          filter: { [key]: filter },
+          sort: null,
+        };
+        return fetchFilterMembers(payload);
+      })
+    ).then((array) => {
+      const flattenedArray = array.flatMap((innerArray) => innerArray);
+      const filterResult = flattenedArray.reduce(
+        (accumulator, currentValue) => {
+          const existingObject = accumulator.find(
+            (obj) => obj.name === currentValue.name
+          );
+          if (!existingObject) {
+            accumulator.push(currentValue);
+          }
+          return accumulator;
         },
-        {
-          name: "cellphone",
-          label: "手機",
-          align: "left",
-          field: "cellphone",
-        },
-        {
-          name: "email",
-          label: "信箱",
-          align: "left",
-          field: "email",
-        },
-        {
-          name: "gender",
-          label: "性別",
-          align: "left",
-          field: "gender",
-        },
-        {
-          name: "birthday",
-          label: "生日",
-          align: "left",
-          field: "birthday",
-        },
-      ],
-      rows: [],
+        []
+      );
+      return filterResult;
     });
+  } else {
+    return originData.value;
+  }
+}
+function formatBirthday(value) {
+  return date.formatDate(
+    value.replace(/(\d{4})-(\d{1,2})-(\d{1,2})/, (match, year, month, day) => {
+      return (
+        year +
+        "-" +
+        (month.length === 1 ? "0" + month : month) +
+        "-" +
+        (day.length === 1 ? "0" + day : day)
+      );
+    }),
+    "YYYY-MM-DD"
+  );
+}
 
-    return {
-      state,
-    };
-  },
-};
+function getSelectedString() {
+  return "";
+}
+function onCancelAdd() {
+  isAddDialog.value = false;
+}
+function onConfirmAdd(data) {
+  isAddDialog.value = false;
+  originData.value.unshift(data);
+  state.rows = [...originData.value];
+}
+function onCancelDelete() {
+  isDeleteDialog.value = false;
+}
+function onConfirmDelete(data) {
+  originData.value = originData.value.filter((item1) => {
+    return !data.some((item2) => item2.name === item1.name);
+  });
+  state.rows = [...originData.value];
+  selected.value = [];
+  isDeleteDialog.value = false;
+}
+function onFilter(input) {
+  filter.value = input;
+  loading.value = true;
+  setTimeout(async () => {
+    // api搜尋，暫用本地搜尋故註解
+    // const returnedData = await fetchFromServer(filter.value);
+    // state.rows = returnedData.map((item) => {
+    //   return { ...item, birthday: formatBirthday(item.birthday) };
+    // });
+
+    loading.value = false;
+  }, 1500);
+}
 </script>
 <style lang="scss" scoped>
 .block-item {
